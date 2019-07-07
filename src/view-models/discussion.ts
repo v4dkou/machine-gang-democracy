bff918d8a30931c6427157c1fd3d861448d99bd2
Dmitry Glinyanov <gliv321@gmail.com>
2019-07-07 10:22:06 +0700

    Added initial discussion-create screen.

--- src/view-models/discussion.ts
+++ src/view-models/discussion.ts
@@ -3,7 +3,7 @@ import { createContentStore } from '../../components/content-store'
 import { FormField, validate } from '../../components/fields/form-field'
 import { StringEmptyOptions } from '../../components/fields/string-options'
 import { ConnectionError } from '../../components/utils/error-utils'
-import { Note } from '../services/database/schemas/discussion'
+import { DiscussionTopic } from '../services/api/api'
 import { DiscussionStore } from '../stores/discussion-store'
 import { T } from '../style/values'
 
@@ -11,87 +11,76 @@ export class DiscussionViewModel {
   @observable
   public discussionId: number
 
-  public readonly title = new FormField(
-    new StringEmptyOptions(T.string.empty_title_text),
-  )
   public readonly description = new FormField(
     new StringEmptyOptions(T.string.empty_description_text),
   )
 
-  public readonly saveNoteRequest = createContentStore<Note, ConnectionError>()
-  public readonly deleteNoteRequest = createContentStore<
+  public readonly saveDiscussionRequest = createContentStore<DiscussionTopic, ConnectionError>()
+  public readonly deleteDiscussionRequest = createContentStore<
     void,
     ConnectionError
   >()
 
   constructor(private readonly discussionStore: DiscussionStore, discussionId: number) {
     this.discussionId = discussionId
-    this.updateNoteData()
+    this.updateDiscussionData()
   }
 
-  @computed get discussion(): Note {
+  @computed get discussion(): DiscussionTopic {
     if (this.discussionId === undefined || this.discussionId === null) return null
 
     return this.discussionStore.discussionList.find(item => item.id === this.discussionId)
-  }
-
-  public setTitle = (title: string) => {
-    this.title.setData(title)
   }
 
   public setDescription = (description: string) => {
     this.description.setData(description)
   }
 
-  // public savePressed = () => {
-  //   if (validate(this.title, this.description)) {
-  //     let savePromise
-  //     if (this.discussionId) {
-  //       if (
-  //         this.title.data === this.discussion.title &&
-  //         this.description.data === this.discussion.description
-  //       ) {
-  //         this.title.setError(T.string.no_changes_discussion_error)
-  //         return
-  //       }
-  //
-  //       savePromise = this.discussionStore.updateDiscussion(
-  //         this.discussionId,
-  //         this.title.data,
-  //         this.description.data,
-  //       )
-  //     } else {
-  //       savePromise = this.discussionStore.createDiscussion(
-  //         this.title.data,
-  //         this.description.data,
-  //       )
-  //     }
-  //
-  //     this.saveNoteRequest.setLoading()
-  //
-  //     savePromise
-  //       .then(this.saveNoteRequest.setData)
-  //       .catch(this.saveNoteRequest.setError)
-  //   }
-  // }
+  public savePressed = () => {
+    if (validate(this.description)) {
+      let savePromise
+      if (this.discussionId) {
+        if (
+          this.description.data === this.discussion.description
+        ) {
+          this.description.setError(T.string.no_changes_note_error)
+          return
+        }
+
+        // savePromise = this.discussionStore.updateDiscussion(
+        //   this.discussionId,
+        //   this.description.data,
+        // )
+      } else {
+        savePromise = this.discussionStore.createDiscussion(
+          this.description.data,
+        )
+      }
+
+      this.saveDiscussionRequest.setLoading()
+
+      savePromise
+        .then(this.saveDiscussionRequest.setData)
+        .catch(this.saveDiscussionRequest.setError)
+    }
+  }
 
   // public deletePressed = () => {
-  //   this.deleteNoteRequest.setLoading()
+  //   this.deleteDiscussionRequest.setLoading()
   //
   //   this.discussionStore
-  //     .deleteNote(this.discussion)
-  //     .then(this.deleteNoteRequest.setData)
-  //     .catch(this.deleteNoteRequest.setError)
+  //     .deleteDiscussionTopic(this.discussion)
+  //     .then(this.deleteDiscussionRequest.setData)
+  //     .catch(this.deleteDiscussionRequest.setError)
   // }
 
   @action
-  public updateNoteId(discussionId: number) {
+  public updateDiscussionId(discussionId: number) {
     this.discussionId = discussionId
   }
 
-  public updateNoteData() {
+  public updateDiscussionData() {
     if (this.discussion) {
-      this.title.setData(this.discussion.title)
       this.description.setData(this.discussion.description)
     }
   }
